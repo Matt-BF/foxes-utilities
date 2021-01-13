2149d391ab21f981db5b40ff8fca89b29231ef6c
Matt <mbfiamenghi@gmail.com>
2021-01-13 12:41:46 -0300

    import chromedriver binary

--- celery-queue/tasks.py
+++ celery-queue/tasks.py
@@ -4,6 +4,7 @@ from celery import Celery
 from auto_worklab_chrome import auto_laudo
 from analyze_covid import analyze_csv
 import time
+import chromedriver_binary
 
 CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379')
 CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379')
@@ -12,6 +13,6 @@ CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localho
 celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
 
 @celery.task(bind=True)
-def start_auto_laudo(self,table_name, chromedriver_path):
+def start_auto_laudo(self,table_name):
     table = analyze_csv(table_name)
-    auto_laudo(table, chromedriver_path, headless=False, validate=True)
+    auto_laudo(table, headless=False, validate=True)
