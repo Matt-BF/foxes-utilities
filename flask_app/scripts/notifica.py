ea556ab86e419213858ab157ce29fd2912f02021
Matt_BF <mbfiamenghi@gmail.com>
2021-03-17 14:11:18 -0300

    changed email credentials

--- flask_app/scripts/notifica.py
+++ flask_app/scripts/notifica.py
@@ -57,7 +57,7 @@ def send_mail(df, day2):
 
     smtpObj = smtplib.SMTP("smtp.gmail.com", 587)
     smtpObj.starttls()
-    smtpObj.login("cientifico@foxesbm.com", "HEqj245tRhkBNGg")
+    smtpObj.login("mateus.fiamenghi@foxesbm.com", "HEqj245tRhkBNGg")
     msg = MIMEMultipart()
     msg["Subject"] = "Notificações Covid-19 - FoxES"
     msg_text = f"""Boa noite,
